57432bad20276fb98e987f020ffb0275ddc34a07
binyaminCh <92228137+binya2@users.noreply.github.com>
2023-06-05 21:55:05 +0300

    Update 5.4.interleave.py
    rename 'i'

--- week5/5.4.interleave.py
+++ week5/5.4.interleave.py
@@ -3,14 +3,14 @@ def interleave(*lists):
     Takes one or more lists as input and returns a new list with the interwoven members of all input lists.
     """
     max_length = max(len(lst) for lst in lists)
-    for i in range(max_length):
+    for index in range(max_length):
         for lst in lists:
-            if i < len(lst):
+            if index < len(lst):
                 yield lst[i]
 
 
 if __name__ == '__main__':
     result = []
-    for i in interleave('abc', [1, 2, 3, 4], ('!', '@', '#')):
-        result.append(i)
+    for element  in interleave('abc', [1, 2, 3, 4], ('!', '@', '#')):
+        result.append(element )
     print(result)
