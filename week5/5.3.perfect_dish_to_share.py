72da2f273dbe0abdf3d53b60e807d5dd3373b915
binyaminCh <92228137+binya2@users.noreply.github.com>
2023-06-05 22:07:42 +0300

    Update 5.3.perfect_dish_to_share.py
    Rename 'i' and 'n'

--- week5/5.3.perfect_dish_to_share.py
+++ week5/5.3.perfect_dish_to_share.py
@@ -1,9 +1,9 @@
-def is_perfect_number(n):
+def is_perfect_number(value):
     """
-    Returns True if n is a perfect number, the sum of the proper divisors of n is equal to n.
+    Returns True if value is a perfect number, the sum of the proper divisors of n is equal to value.
     """
-    divisors = [i for i in range(1, n) if n % i == 0 and i != n]
-    return sum(divisors) == n
+    divisors = [num  for num  in range(1, value ) if n % num  == 0 and i != value ]
+    return sum(divisors) == value
 
 
 if __name__ == '__main__':
