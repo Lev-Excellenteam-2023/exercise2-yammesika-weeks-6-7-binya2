c328000d6d0a59d0c4982d8a9a586ffbf45c3554
Binyamin chouraqui <92228137+binya2@users.noreply.github.com>
2023-08-24 17:17:24 +0300

    Update 6.2.2000ranning.py
    Correction according to the comments

--- week6/6.2.2000ranning.py
+++ week6/6.2.2000ranning.py
@@ -7,14 +7,31 @@ import time
     :return: the execution time of the function
     """
 
+def timer_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        elapsed_time = time.time() - start_time
+        print(f"Function '{func.__name__}' took {elapsed_time:.4f} seconds to execute.")
+        return result
+    return wrapper
 
-def timer(f, *args, **kwargs):
-    start_time = time.time()
-    f(*args, **kwargs)
-    return time.time() - start_time
+@timer_decorator
+def function_1(message):
+    print(message)
 
+@timer_decorator
+def function_2(list1, list2):
+    result = list(zip(list1, list2))
+    print(result)
+
+@timer_decorator
+def function_3(name):
+    result = "Hi {name}".format(name=name)
+    print(result)
 
 if __name__ == '__main__':
-    print(timer(print, "Hello"))
-    print(timer(zip, [1, 2, 3], [4, 5, 6]))
-    print(timer("Hi {name}".format, name="Bug"))
+    function_1("Hello")
+    function_2([1, 2, 3], [4, 5, 6])
+    function_3(name="Bug")
+
